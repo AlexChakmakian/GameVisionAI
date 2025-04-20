fa8f1e82b9a4f0f2a4e936809f7631bce43996ca
cflores124568 <48939920+cflores124568@users.noreply.github.com>
2025-04-20 12:38:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
+from view_transformer import ViewTransformer
+from speed_and_distance_estimator import SpeedAndDistance_Estimator
 
 def main():
     # Read Video
@@ -12,7 +14,7 @@ def main():
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
-
+    #Set read_from_stub=False to fix IndexError: list index out of range
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
@@ -26,6 +28,10 @@ def main():
                                                                                 read_from_stub=True,
                                                                                 stub_path='stubs/camera_movement_stub.pkl')
     camera_movement_estimator.add_adjust_positions_to_tracks(tracks, camera_movement_per_frame)
+
+    #View Transformer
+    view_transformer = ViewTransformer()
+    view_transformer.add_transformed_position_to_tracks(tracks)
 
     # Interpolate Ball Positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
@@ -64,6 +70,9 @@ def main():
 
     ## Draw Camera Movement
     output_video_frames = camera_movement_estimator.draw_camera_movement(output_video_frames, camera_movement_per_frame)
+
+    ##Draw Speed and Distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
 
     # Save Video
     save_video(output_video_frames, 'output_videos/output_video.avi')
